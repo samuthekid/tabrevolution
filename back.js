a5e8938fc8cd77f5997b3eb78a0d1bac78a99168
samuthekid@gmail.com <snunes@student.dei.uc.pt>
2017-02-18 01:04:10 +0000

    wow

--- back.js
+++ back.js
@@ -1,14 +1,73 @@
-
+var stack;
 
 function init(){
-	// Start here
+    // Start here
+    console.log("back running");
+    chrome.tabs.query({},function(tab){
+        stack = tab;
+    });
 }
 
-chrome.runtime.onMessage.addListener(function(response, tab, callback){
-    if(response.code == "getData")
-		callback({data: "teste"});
+function broadcast(data,sender){
+    chrome.tabs.query({},function(tab){
+        $.each(tab,function(i,x){
+            if(x.id!=sender)
+                chrome.tabs.sendMessage(x.id,data);
+        });
+    });
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    chrome.tabs.query({},function(tab){
+        stack = tab;
+    });
 });
 
-chrome.tabs.sendMessage();
+chrome.tabs.onCreated.addListener(function(tab){
+    console.log("onCreated activated");
+});
+
+chrome.tabs.onMoved.addListener(function(tabId,moveInfo){
+    console.log("onMoved activated");
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo){
+    console.log("onActivated activated");
+});
+
+chrome.tabs.onHighlighted.addListener(function(highlightInfo){
+    console.log("onHighlighted activated");
+});
+
+chrome.tabs.onDetached.addListener(function(tabId,detachInfo){
+    console.log("onDetached activated");
+});
+
+chrome.tabs.onAttached.addListener(function(tabId,attachInfo){
+    console.log("onAttached activated");
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId,removeInfo){
+    console.log("onRemoved activated");
+});
+
+chrome.tabs.onReplaced.addListener(function(addedTabId,removedTabId){
+    console.log("onReplaced activated");
+});
+
+
+chrome.runtime.onMessage.addListener(
+    function(response, tab, callback){
+        if(response.code == "getData"){
+            console.log(tab.tab);
+            callback({stack: stack,
+                      myId: tab.tab.id,
+                      windowId: tab.tab.windowId});
+        }else{
+            console.log("broadcast");
+            console.log(response);
+            broadcast(response,tab.id);
+        }
+});
 
 init();
--- main.js
+++ main.js
@@ -1,10 +1,48 @@
-$(document).on("load", main.load());
-$(document).on("keydown", main.shortcutOptions);
-$(document).on("keyup", main.shortcutOptionsExit);
+var stack;
+var myId;
+var windowId;
 
-$(window).on("blur", main.shortcutOptionsForce);
-$(window).on("load", function(){$("#search_box").focus();});
-$(window).on("focus", function(){$("#search_box").focus();});
+function init(){
+    // Start here
+    console.log("front running");
+    chrome.runtime.sendMessage({"code":"getData"},
+        function(response){
+            stack = response.stack;
+            myId = response.myId;
+            windowId = response.windowId;
+            console.log(myId+" "+windowId);
+            redraw();
+    });
 
+    $("head").append("<style id='tr_style'></style>");
+    $("#tr_style").load(chrome.extension.getURL("/style.css"));
+    
+    $("body").append("<div id='tr'></div>");
+    
+    console.log("DONE");
+}
 
-chrome.runtime.sendMessage({code: "get"},main.init);
+function redraw(){
+    $.each(stack,function(i,x){
+        if(x.windowId == windowId){
+            t = $("<div>");
+            t.addClass("tr_tab");
+            //t.append("<img class='tr_favicon' src='"+tab.favIconUrl+"'/>");
+            if(x.id == myId)
+                t.addClass("tr_this");
+            t.text(x.title);
+            console.log(t);
+            $("#tr").append(t);
+        }
+    });
+}
+
+chrome.runtime.onMessage.addListener(
+    function(response, tab, callback){
+        console.log(response);
+        console.log(tab);
+        console.log(callback);
+    }
+);
+
+$(document).on("load", init());
